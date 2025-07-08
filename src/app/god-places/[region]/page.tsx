98968adff7e7a5c40fd49f88771f8ebbea6d95d4
manto123123123 <lhw971103@gmail.com>
2025-07-08 09:58:30 +0900

    :Start 프로젝트 초기 세팅

--- src/app/god-places/[region]/page.tsx
+++ src/app/god-places/[region]/page.tsx
@@ -1,4 +1,8 @@
-export default async function page({ params }: { params: Promise<{ region: string }> }) {
+export default async function page({
+  params,
+}: {
+  params: Promise<{ region: string }>;
+}) {
   const { region } = await params;
   return (
     <>
