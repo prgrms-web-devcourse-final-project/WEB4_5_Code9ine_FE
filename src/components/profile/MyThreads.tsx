b8191233e329226f62ecfe07b557d6887328774f
JINJIY23 <dhkswksla22@naver.com>
2025-07-10 20:56:44 +0900

    Style : 쓰레드 영역 퍼블리싱

--- src/components/profile/MyThreads.tsx
+++ src/components/profile/MyThreads.tsx
@@ -1,8 +1,13 @@
 import SetGoal from './SetGoal';
+import PostItem from '../board/PostItem';
 export default function MyThreads() {
   return (
     <>
       <SetGoal />
+      <div className="mx-auto mt-[200px] flex w-full max-w-[720px] flex-col items-center gap-[20px]">
+        <PostItem />
+        <PostItem />
+      </div>
     </>
   );
 }
