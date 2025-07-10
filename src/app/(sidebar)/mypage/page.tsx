cf23952192c4620c66a911e5ceda2ee394f6d45c
JINJIY23 <dhkswksla22@naver.com>
2025-07-10 17:48:31 +0900

    Feature: 챌린지 스와이퍼 기능 구현

--- src/app/(sidebar)/mypage/page.tsx
+++ src/app/(sidebar)/mypage/page.tsx
@@ -1,10 +1,11 @@
 import MyThreads from '@/components/profile/MyThreads';
 import TitleSwiper from '@/components/profile/TitleSwiper';
 import Profile from '@/components/profile/Profile';
+import Challenge from '@/components/profile/Challenge';
 export default function page() {
   return (
     <>
-      <div className="flex gap-[15px]">
+      <div className="flex gap-[10px]">
         <div className="h-[870px] w-[756px] rounded-[10px] bg-[var(--white-color)] shadow-md">
           <MyThreads />
         </div>
@@ -13,7 +14,9 @@ export default function page() {
             <TitleSwiper />
             <Profile />
           </div>
-          <div className="h-[460px] w-[350px] rounded-[10px] bg-[var(--white-color)] shadow-md"></div>
+          <div className="h-[460px] w-[350px] rounded-[10px] bg-[var(--white-color)] shadow-md">
+            <Challenge />
+          </div>
         </div>
       </div>
     </>
