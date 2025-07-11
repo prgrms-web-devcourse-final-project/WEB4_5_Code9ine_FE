0bbb4a3fcc5ffe23b40fce1394c83193133046df
JINJIY23 <dhkswksla22@naver.com>
2025-07-11 22:51:19 +0900

    Style : 모바일/테블릿 반응형 퍼블리싱

--- src/components/profile/Challenge.tsx
+++ src/components/profile/Challenge.tsx
@@ -2,7 +2,7 @@ import Mission from './Mission';
 export default function Challenge() {
   return (
     <>
-      <h1 className="flex items-center justify-center p-[20px] text-[20px] font-semibold">
+      <h1 className="flex items-center justify-center p-[20px] text-[16px] font-semibold md:text-[20px]">
         챌린지
       </h1>
       <Mission />
--- src/components/profile/SetGoal.tsx
+++ src/components/profile/SetGoal.tsx
@@ -1,14 +1,19 @@
+import { MdEdit } from 'react-icons/md';
 export default function SetGoal() {
   return (
     <>
-      <div className="relative mt-[20px] flex h-[40px] w-full items-center justify-center bg-[var(--main-color-1)]">
-        <h1 className="text-[20px] dark:text-[#2b2e34]">
+      <div className="relative mt-[20px] flex h-[40px] w-full items-center justify-start bg-[var(--main-color-1)] md:items-center md:justify-center">
+        <h1 className="ml-[10px] gap-[40px] text-[16px] md:text-[20px] dark:text-[#2b2e34]">
           유럽 여행까지{' '}
           <span className="text-[var(--main-color-3)]">300만원</span> 남았어요.
           끝까지 화이팅!
         </h1>
-        <button className="absolute right-3 cursor-pointer text-[var(--white-color)] dark:text-[var(--text-color)]">
+        <button className="absolute right-3 hidden cursor-pointer text-[var(--white-color)] hover:text-[var(--main-color-3)] md:block dark:text-[var(--text-color)]">
           목표 바꾸기
+        </button>
+
+        <button className="absolute right-1 block cursor-pointer text-[var(--white-color)] hover:text-[var(--main-color-3)] md:hidden dark:text-[var(--text-color)]">
+          <MdEdit size={20} />
         </button>
       </div>
     </>
