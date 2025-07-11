705a1ee42b7077a1673646625bafed471adad0cc
CHOIYEONSEO <yeonseochoi0717@gmail.com>
2025-07-11 13:31:39 +0900

    feat: 갓플 페이지 카테고리 box css 수정

--- src/components/godplaces/search/CategoryBox.tsx
+++ src/components/godplaces/search/CategoryBox.tsx
@@ -8,15 +8,13 @@ export default function CategoryBox({
 }) {
   const divSizeVariants = {
     beforeSearch:
-      'gap-x-[7px] gap-y-[8px] md:gap-[15px] h-[59px] w-[253px] md:h-[37px] md:w-[727px] grid-cols-[auto_auto_auto_auto_auto]',
+      'gap-x-[7px] gap-y-[8px] md:gap-[15px] h-[59px] w-[253px] md:h-[37px] md:w-[727px] grid-cols-[auto_auto_auto_auto_auto] place-content-center',
     afterSearch:
-      'gap-x-[7px] gap-y-[5px] md:gap-[10px] h-[59px] md:h-[37px] w-[291px] md:w-[687px] grid-cols-[auto_auto_auto_auto_auto]',
+      'gap-x-[7px] gap-y-[8px] mb-[3px] md:mb-0 md:gap-[10px] h-[59px] md:h-[37px] w-[300px] md:w-[687px] grid-cols-[auto_auto_auto_auto_auto_auto]',
   };
 
   return (
-    <div
-      className={`grid place-content-center md:flex ${divSizeVariants[classType]}`}
-    >
+    <div className={`grid md:flex ${divSizeVariants[classType]}`}>
       {categories.category.map((data) => (
         <CategoryItem key={data.id} type={data.type} classType={classType} />
       ))}
