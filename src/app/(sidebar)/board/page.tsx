e59d44d7d576c74164f8ed098f5ae83a8e0bd9c8
mafornp <wjddntjems@gmail.com>
2025-07-11 11:21:56 +0900

    Style: 커뮤니티 탭 추가

--- src/app/(sidebar)/board/page.tsx
+++ src/app/(sidebar)/board/page.tsx
@@ -1,15 +1,27 @@
 'use client';
 
+import CommunityTab from '@/components/board/CommunityTab';
 import PopularPostList from '@/components/board/PopularPostList';
 import PostItem from '@/components/board/PostItem';
 import PostWriteForm from '@/components/board/PostWriteForm';
+import { useState } from 'react';
 
-export default function page() {
+export default function Page() {
+  const [selectedTab, setSelectedTab] = useState<
+    'myHiddenStore' | 'challenge' | 'freeBoard'
+  >('myHiddenStore');
+
   return (
     <>
       <div className="flex">
         <div className="hide-scrollbar flex h-[869px] w-[756px] flex-col overflow-y-auto rounded-[10px] bg-[var(--white-color)] p-4 shadow">
           <div className="mb-[25px]">
+            <div className="mt-[15px] mb-[30px]">
+              <CommunityTab
+                selectedTab={selectedTab}
+                onChange={setSelectedTab}
+              />
+            </div>
             <PostWriteForm />
           </div>
           <div className="mb-[25px]">
