9789777ef147ee1841a14fba1acdf5c252532b39
manto123123123 <lhw971103@gmail.com>
2025-07-11 14:17:41 +0900

    :Style 메인페이지,헤더 퍼블리싱

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,12 @@
 import Loginbox from '@/components/login/loginbox';
+import loginbg from '@/assets/loginbg.png';
 
 export default function Login() {
   return (
-    <div className="flex h-[900px] w-[1366px] items-center justify-center bg-[url('/loginbg.png')]">
+    <div
+      className="flex h-[900px] w-[1366px] items-center justify-center rounded-[20px] bg-cover bg-center"
+      style={{ backgroundImage: `url(${loginbg.src})` }}
+    >
       <Loginbox />
     </div>
   );
