c34ee338fd9ad90cb30ea391f98eab69ba540292
SeolWolWha <leeyoon97@naver.com>
2025-07-13 07:25:59 +0900

    Design: 마진 수정

--- src/app/(sidebar)/godplaces/[region]/@results/page.tsx
+++ src/app/(sidebar)/godplaces/[region]/@results/page.tsx
@@ -2,7 +2,7 @@ import SearchListBox from '@/components/godplaces/search/SearchListBox';
 
 export default function ResultsPage() {
   return (
-    <div className="mx-[15px] mb-[15px] flex-1 flex min-w-[330px] flex-col rounded-[10px] bg-[var(--white-color)] shadow-[var(--shadow-md)] md:mx-[0px] md:mt-0 md:mb-[0px] md:h-[870px] md:w-[350px]">
+    <div className="mb-[15px] flex-1 flex min-w-[330px] flex-col rounded-[10px] bg-[var(--white-color)] shadow-[var(--shadow-md)] md:mx-[0px] md:mt-0 md:mb-[0px] md:h-[870px] md:w-[350px]">
       <SearchListBox />
     </div>
   );
