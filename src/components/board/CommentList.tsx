558c1e05e95f4d85cb25045c8d379e6bfac11e9d
mafornp <wjddntjems@gmail.com>
2025-07-13 17:15:42 +0900

    Style: 게시판 반응형

--- src/components/board/CommentList.tsx
+++ src/components/board/CommentList.tsx
@@ -41,16 +41,16 @@ export default function CommentList() {
 
   return (
     <div className="mt-[10px] text-[var(--text-color-white)]">
-      <form className="mb-4 flex items-center gap-2 py-2">
+      <form className="mb-4 flex items-center py-2">
         <input
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder="댓글을 입력하세요"
-          className="flex w-[487px] rounded-[10px] border-2 border-[var(--main-color-1)] bg-[var(--white-color)] px-4 py-2 outline-none focus:border-[var(--main-color-2)]"
+          className="mr-[10px] flex w-full rounded-[10px] border-2 border-[var(--main-color-1)] bg-[var(--white-color)] px-4 py-2 outline-none focus:border-[var(--main-color-2)] md:w-[500px]"
         />
         <button
           type="submit"
-          className="h-[28px] w-[58px] cursor-pointer rounded-[20px] bg-[var(--main-color-1)] text-[16px] text-black transition-colors hover:bg-[var(--main-color-2)]"
+          className="h-[28px] w-[48px] shrink-0 cursor-pointer rounded-[20px] bg-[var(--main-color-1)] text-[14px] text-black transition-colors hover:bg-[var(--main-color-2)] md:w-[58px] md:text-[16px]"
         >
           작성
         </button>
@@ -84,7 +84,7 @@ export default function CommentList() {
                 {c.content}
               </span>
 
-              <button className="h-[28px] w-[58px] rounded-[20px] bg-[var(--point-color-1)] text-[16px] text-black transition-colors hover:bg-[var(--point-color-2)]">
+              <button className="h-[28px] w-[48px] shrink-0 rounded-[20px] bg-[var(--point-color-1)] text-[14px] text-black transition-colors hover:bg-[var(--point-color-2)] md:w-[58px] md:text-[16px]">
                 삭제
               </button>
             </div>
--- src/components/board/PostWriteForm.tsx
+++ src/components/board/PostWriteForm.tsx
@@ -4,22 +4,27 @@ import boardUploadIcon from '../../assets/board_upload_image.svg';
 
 export default function PostWriteForm() {
   return (
-    <div className="flex w-full max-w-[724px] items-start gap-4 rounded-[10px] bg-[var(--background)] p-4 text-[20px] text-[var(--text-color-white)] shadow">
-      <div className="flex max-w-[120px] min-w-[80px] flex-col items-center justify-center">
+    <div className="flex w-full max-w-[724px] flex-col items-start gap-4 rounded-[10px] bg-[var(--background)] p-4 text-[20px] text-[var(--text-color-white)] shadow md:flex-row">
+      <div className="flex min-w-[80px] flex-row items-center justify-center md:w-[120px] md:flex-col">
         <Image
           src="/profileTest.png"
           alt="프로필"
           width={70}
           height={70}
-          className="rounded-full border-2 border-[var(--main-color-2)]"
+          className="h-[30px] w-[30px] rounded-full border-2 border-[var(--main-color-2)] md:h-[70px] md:w-[70px]"
         />
-        <div className="mt-2 text-center text-[20px] font-normal">스펀지밥</div>
-        <div className="text-center text-[16px] font-normal text-[var(--text-color-2)]">
-          노노커피 마스터
+
+        <div className="flex flex-row items-baseline gap-1 whitespace-nowrap md:flex-col md:items-center">
+          <div className="ml-[8px] text-center text-[20px] leading-none">
+            스펀지밥
+          </div>
+          <div className="text-center text-[14px] leading-none text-[var(--text-color-2)] md:text-[16px]">
+            노노커피 마스터
+          </div>
         </div>
       </div>
 
-      <div className="flex flex-1 flex-col gap-2">
+      <div className="flex w-full flex-col gap-2">
         <input
           className="focus:border-[var(--main-color-2) h-[50px] w-full rounded-[10px] border border-[var(--main-color-1)] bg-[var(--white-color)] p-4 text-[18px] placeholder:text-[var(--gray-color-2)] focus:border-[var(--main-color-2)] focus:outline-none"
           placeholder="제목을 입력해 주세요."
@@ -37,7 +42,7 @@ export default function PostWriteForm() {
             height={24}
             className="absolute top-[10px] right-[10px] z-10 cursor-pointer opacity-70 hover:opacity-100"
           />
-          <button className="absolute right-[10px] bottom-[10px] z-10 h-[28px] w-[58px] cursor-pointer rounded-[20px] bg-[var(--main-color-1)] text-[16px] text-black transition-colors hover:bg-[var(--main-color-2)]">
+          <button className="absolute right-[10px] bottom-[10px] z-10 h-[28px] w-[58px] cursor-pointer rounded-[20px] bg-[var(--main-color-1)] text-[14px] text-black transition-colors hover:bg-[var(--main-color-2)] md:text-[16px]">
             작성
           </button>
         </div>
