1f3a89ab4338905571766b9d421f8e8144db2cae
CHOIYEONSEO <yeonseochoi0717@gmail.com>
2025-07-14 09:22:46 +0900

    fix: 레이아웃 수정

--- src/app/(sidebar)/godplaces/[region]/@search/default.tsx
+++ src/app/(sidebar)/godplaces/[region]/@search/default.tsx
@@ -5,9 +5,9 @@ import SearchBox from '@/components/godplaces/search/SearchBox';
 
 export default function page() {
   return (
-    <div className="mini-w-[330px] rounded-[10px] md:h-[870px] md:w-[756px]">
-      <div className="flex flex-col gap-[15px] md:gap-[24px]">
-        <div>
+    <div className="min-w-[330px] rounded-[10px] md:h-[870px] md:w-[756px]">
+      <div className="flex flex-col justify-center gap-[15px] md:mx-[0px] md:mt-[0px] md:gap-[24px]">
+        <div className="flex flex-col gap-[5px] md:gap-[0px]">
           <SearchBox classType="afterSearch" />
           <CategoryBox classType="afterSearch" />
         </div>
