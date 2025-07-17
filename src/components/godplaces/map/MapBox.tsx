66d03b42ca39409a32ca3abf0d9d23a2ee0a4060
CHOIYEONSEO <yeonseochoi0717@gmail.com>
2025-07-17 10:34:41 +0900

    feat: 마커 즐겨찾기 버튼 추가

--- src/components/godplaces/map/MapBox.tsx
+++ src/components/godplaces/map/MapBox.tsx
@@ -1,9 +1,10 @@
 'use client';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { CustomOverlayMap, Map, MapMarker } from 'react-kakao-maps-sdk';
 import useKaKaoLoader from './useKaKaoLoader';
 import markers from '../../../assets/icons/markers.png';
 import { useGodplacesStore } from '@/stores/godplacesStore';
 import { getGodplacePositions } from '@/lib/helper/getGodplacePositions';
+import { BsStarFill } from 'react-icons/bs';
 
 const dummyCenter = { lat: 37.5452442, lng: 127.0473267 };
 // const dummyData = [
@@ -72,150 +73,166 @@ export default function MapBox() {
   const festivalMarkerOrigin = { x: 16, y: 440 };
 
   return (
-    <Map
-      className="h-[230px] min-w-[330px] rounded-[10px] bg-gray-300 shadow-[var(--shadow-md)] md:h-[637px] md:w-[756px]"
-      center={
-        godplacesPositions && godplacesPositions.length > 0
-          ? godplacesPositions[0]
-          : dummyCenter
-      }
-      level={7}
-    >
-      {koreanRestaurantPositions &&
-        koreanRestaurantPositions.map((position) => (
-          <MapMarker
-            key={`krRestaurant-${position.lat},${position.lng}`}
-            position={position}
-            image={{
-              src: markers.src,
-              size: markerSize,
-              options: {
-                spriteSize: spriteImageSize,
-                spriteOrigin: koreanRestaurantMarkerOrigin,
-              },
-            }}
-          ></MapMarker>
-        ))}
-      {chineseRestaurantPositions &&
-        chineseRestaurantPositions.map((position) => (
-          <MapMarker
-            key={`krRestaurant-${position.lat},${position.lng}`}
-            position={position}
-            image={{
-              src: markers.src,
-              size: markerSize,
-              options: {
-                spriteSize: spriteImageSize,
-                spriteOrigin: chineseRestaurantMarkerOrigin,
-              },
-            }}
-          ></MapMarker>
-        ))}
-      {japaneseRestaurantPositions &&
-        japaneseRestaurantPositions.map((position) => (
-          <MapMarker
-            key={`krRestaurant-${position.lat},${position.lng}`}
-            position={position}
-            image={{
-              src: markers.src,
-              size: markerSize,
-              options: {
-                spriteSize: spriteImageSize,
-                spriteOrigin: japaneseRestaurantMarkerOrigin,
-              },
-            }}
-          ></MapMarker>
-        ))}
-      {westernRestaurantPositions &&
-        westernRestaurantPositions.map((position) => (
-          <MapMarker
-            key={`krRestaurant-${position.lat},${position.lng}`}
-            position={position}
-            image={{
-              src: markers.src,
-              size: markerSize,
-              options: {
-                spriteSize: spriteImageSize,
-                spriteOrigin: westernRestaurantMarkerOrigin,
-              },
-            }}
-          ></MapMarker>
-        ))}
-      {libraryPositions &&
-        libraryPositions.map((position) => (
-          <MapMarker
-            key={`krRestaurant-${position.lat},${position.lng}`}
-            position={position}
-            image={{
-              src: markers.src,
-              size: markerSize,
-              options: {
-                spriteSize: spriteImageSize,
-                spriteOrigin: libraryMarkerOrigin,
-              },
-            }}
-          ></MapMarker>
-        ))}
-      {beautySalonPositions &&
-        beautySalonPositions.map((position) => (
-          <MapMarker
-            key={`krRestaurant-${position.lat},${position.lng}`}
-            position={position}
-            image={{
-              src: markers.src,
-              size: markerSize,
-              options: {
-                spriteSize: spriteImageSize,
-                spriteOrigin: beautySalonMarkerOrigin,
-              },
-            }}
-          ></MapMarker>
-        ))}
-      {laundryPositions &&
-        laundryPositions.map((position) => (
-          <MapMarker
-            key={`krRestaurant-${position.lat},${position.lng}`}
-            position={position}
-            image={{
-              src: markers.src,
-              size: markerSize,
-              options: {
-                spriteSize: spriteImageSize,
-                spriteOrigin: laundryMarkerOrigin,
-              },
-            }}
-          ></MapMarker>
-        ))}
-      {hotelPositions &&
-        hotelPositions.map((position) => (
-          <MapMarker
-            key={`krRestaurant-${position.lat},${position.lng}`}
-            position={position}
-            image={{
-              src: markers.src,
-              size: markerSize,
-              options: {
-                spriteSize: spriteImageSize,
-                spriteOrigin: hotelMarkerOrigin,
-              },
-            }}
-          ></MapMarker>
-        ))}
-      {festivalPositions &&
-        festivalPositions.map((position) => (
-          <MapMarker
-            key={`krRestaurant-${position.lat},${position.lng}`}
-            position={position}
-            image={{
-              src: markers.src,
-              size: markerSize,
-              options: {
-                spriteSize: spriteImageSize,
-                spriteOrigin: festivalMarkerOrigin,
-              },
-            }}
-          ></MapMarker>
-        ))}
-    </Map>
+    <>
+      {/* <MarkerWithCustomOverlayStyle/> */}
+      <Map
+        className="h-[230px] min-w-[330px] rounded-[10px] bg-gray-300 shadow-[var(--shadow-md)] md:h-[637px] md:w-[756px]"
+        center={
+          godplacesPositions && godplacesPositions.length > 0
+            ? godplacesPositions[0]
+            : dummyCenter
+        }
+        level={7}
+      >
+        {koreanRestaurantPositions &&
+          koreanRestaurantPositions.map((position) => (
+            <div key={`krRestaurant-${position.lat},${position.lng}`}>
+              <MapMarker
+                position={position}
+                image={{
+                  src: markers.src,
+                  size: markerSize,
+                  options: {
+                    spriteSize: spriteImageSize,
+                    spriteOrigin: koreanRestaurantMarkerOrigin,
+                  },
+                }}
+              />
+              <CustomOverlayMap position={position} yAnchor={2.3}>
+                <div className="customoverlay">
+                  <button className="cursor-pointer">
+                    <BsStarFill
+                      fill="white"
+                      stroke="var(--point-color-1)"
+                      strokeWidth={2}
+                      size={20}
+                    />
+                  </button>
+                </div>
+              </CustomOverlayMap>
+            </div>
+          ))}
+        {chineseRestaurantPositions &&
+          chineseRestaurantPositions.map((position) => (
+            <MapMarker
+              key={`krRestaurant-${position.lat},${position.lng}`}
+              position={position}
+              image={{
+                src: markers.src,
+                size: markerSize,
+                options: {
+                  spriteSize: spriteImageSize,
+                  spriteOrigin: chineseRestaurantMarkerOrigin,
+                },
+              }}
+            />
+          ))}
+        {japaneseRestaurantPositions &&
+          japaneseRestaurantPositions.map((position) => (
+            <MapMarker
+              key={`krRestaurant-${position.lat},${position.lng}`}
+              position={position}
+              image={{
+                src: markers.src,
+                size: markerSize,
+                options: {
+                  spriteSize: spriteImageSize,
+                  spriteOrigin: japaneseRestaurantMarkerOrigin,
+                },
+              }}
+            />
+          ))}
+        {westernRestaurantPositions &&
+          westernRestaurantPositions.map((position) => (
+            <MapMarker
+              key={`krRestaurant-${position.lat},${position.lng}`}
+              position={position}
+              image={{
+                src: markers.src,
+                size: markerSize,
+                options: {
+                  spriteSize: spriteImageSize,
+                  spriteOrigin: westernRestaurantMarkerOrigin,
+                },
+              }}
+            />
+          ))}
+        {libraryPositions &&
+          libraryPositions.map((position) => (
+            <MapMarker
+              key={`krRestaurant-${position.lat},${position.lng}`}
+              position={position}
+              image={{
+                src: markers.src,
+                size: markerSize,
+                options: {
+                  spriteSize: spriteImageSize,
+                  spriteOrigin: libraryMarkerOrigin,
+                },
+              }}
+            />
+          ))}
+        {beautySalonPositions &&
+          beautySalonPositions.map((position) => (
+            <MapMarker
+              key={`krRestaurant-${position.lat},${position.lng}`}
+              position={position}
+              image={{
+                src: markers.src,
+                size: markerSize,
+                options: {
+                  spriteSize: spriteImageSize,
+                  spriteOrigin: beautySalonMarkerOrigin,
+                },
+              }}
+            />
+          ))}
+        {laundryPositions &&
+          laundryPositions.map((position) => (
+            <MapMarker
+              key={`krRestaurant-${position.lat},${position.lng}`}
+              position={position}
+              image={{
+                src: markers.src,
+                size: markerSize,
+                options: {
+                  spriteSize: spriteImageSize,
+                  spriteOrigin: laundryMarkerOrigin,
+                },
+              }}
+            />
+          ))}
+        {hotelPositions &&
+          hotelPositions.map((position) => (
+            <MapMarker
+              key={`krRestaurant-${position.lat},${position.lng}`}
+              position={position}
+              image={{
+                src: markers.src,
+                size: markerSize,
+                options: {
+                  spriteSize: spriteImageSize,
+                  spriteOrigin: hotelMarkerOrigin,
+                },
+              }}
+            />
+          ))}
+        {festivalPositions &&
+          festivalPositions.map((position) => (
+            <MapMarker
+              key={`krRestaurant-${position.lat},${position.lng}`}
+              position={position}
+              image={{
+                src: markers.src,
+                size: markerSize,
+                options: {
+                  spriteSize: spriteImageSize,
+                  spriteOrigin: festivalMarkerOrigin,
+                },
+              }}
+            />
+          ))}
+      </Map>
+    </>
   );
 }
