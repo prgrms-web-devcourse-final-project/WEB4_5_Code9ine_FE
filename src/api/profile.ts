063e81df704a8ac12ecba9d8cedf5f1dcd49cdf2
JINJIY23 <dhkswksla22@naver.com>
2025-07-27 11:43:29 +0900

    Feature : 무한스크롤, top버튼

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -98,9 +98,12 @@ export async function setGoal(
 }
 
 // 내가 쓴 글
-export async function getMyThreads(): Promise<BookmarkPostData> {
+export async function getMyThreads(
+  page: number,
+  size: number,
+): Promise<BookmarkPostData> {
   const res = await fetch(
-    `${API_BASE_URL}/api/members/mypage/posts?page=0&size=10`,
+    `${API_BASE_URL}/api/members/mypage/posts?page=${page}&size=${size}`,
     {
       method: 'GET',
       credentials: 'include',
