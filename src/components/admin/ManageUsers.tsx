769e1ddab7aed8b48ec07a9641529d6bb164cb31
CHOIYEONSEO <yeonseochoi0717@gmail.com>
2025-07-29 03:36:47 +0900

    feat: 유저 닉네임으로 검색했을 경우 페이지 버튼 숨김 처리

--- src/components/admin/ManageUsers.tsx
+++ src/components/admin/ManageUsers.tsx
@@ -17,6 +17,7 @@ export default function ManageUsers() {
   const [selectedUserPerPage, setSelectedUserPerPage] = useState(10);
   const [username, setUsername] = useState('');
   const [totalData, setTotalData] = useState(0);
+  const [isPageButtonShow, setIsPageButtonShow] = useState(true);
 
   const fetchAllUsers = async () => {
     try {
@@ -64,14 +65,18 @@ export default function ManageUsers() {
 
   const searchHandler = async (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
-      if (username.trim() === '') await fetchAllUsers();
-      else {
+      if (username.trim() === '') {
+        await fetchAllUsers();
+        setIsPageButtonShow(true);
+      } else {
+        setIsPageButtonShow(false);
         startTransition(async () => {
           const response = await searchUserByNickname(username);
           // console.log(response);
           if (response.code === '0000') {
             setUsers([response.data]);
           } else {
+            setIsPageButtonShow(true);
             setUsers([]);
           }
         });
@@ -126,51 +131,53 @@ export default function ManageUsers() {
           )}
         </div>
 
-        <div className="flex items-center justify-center gap-[10px]">
-          {page > 1 && (
-            <button className="cursor-pointer" onClick={() => setPage(1)}>
-              {'<<'}
-            </button>
-          )}
+        {isPageButtonShow && users.length > 0 && (
+          <div className="flex items-center justify-center gap-[10px]">
+            {page > 1 && (
+              <button className="cursor-pointer" onClick={() => setPage(1)}>
+                {'<<'}
+              </button>
+            )}
 
-          {startPage > 1 && (
-            <button
-              className="cursor-pointer"
-              onClick={() => setPage(startPage - 1)}
-            >
-              {'<'}
-            </button>
-          )}
+            {startPage > 1 && (
+              <button
+                className="cursor-pointer"
+                onClick={() => setPage(startPage - 1)}
+              >
+                {'<'}
+              </button>
+            )}
 
-          {slicedPages.map((item) => (
-            <button
-              key={item}
-              type="button"
-              className={`cursor-pointer ${item === page && 'font-extrabold text-[var(--main-color-2)]'}`}
-              onClick={() => setPage(item)}
-            >
-              {item}
-            </button>
-          ))}
+            {slicedPages.map((item) => (
+              <button
+                key={item}
+                type="button"
+                className={`cursor-pointer ${item === page && 'font-extrabold text-[var(--main-color-2)]'}`}
+                onClick={() => setPage(item)}
+              >
+                {item}
+              </button>
+            ))}
 
-          {endPage < totalPages && (
-            <button
-              className="cursor-pointer"
-              onClick={() => setPage(endPage + 1)}
-            >
-              {'>'}
-            </button>
-          )}
+            {endPage < totalPages && (
+              <button
+                className="cursor-pointer"
+                onClick={() => setPage(endPage + 1)}
+              >
+                {'>'}
+              </button>
+            )}
 
-          {page < totalPages && (
-            <button
-              className="cursor-pointer"
-              onClick={() => setPage(totalPages)}
-            >
-              {'>>'}
-            </button>
-          )}
-        </div>
+            {page < totalPages && (
+              <button
+                className="cursor-pointer"
+                onClick={() => setPage(totalPages)}
+              >
+                {'>>'}
+              </button>
+            )}
+          </div>
+        )}
       </div>
     </>
   );
