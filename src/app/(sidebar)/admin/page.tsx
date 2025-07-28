7c2afd89197126bbadefeeef6fcf3ce147904bbe
CHOIYEONSEO <yeonseochoi0717@gmail.com>
2025-07-29 04:39:09 +0900

    feat: 관리자 페이지 접근 권한 확인 및 404 처리

--- src/app/(sidebar)/admin/page.tsx
+++ src/app/(sidebar)/admin/page.tsx
@@ -1,8 +1,36 @@
+'use client';
+import { getTodayStats } from '@/api/admin';
 import ManageStores from '@/components/admin/ManageStores';
 import ManageUsers from '@/components/admin/ManageUsers';
 import TodayStats from '@/components/admin/TodayStats';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function Page() {
+  const router = useRouter();
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    const checkAccess = async () => {
+      try {
+        const response = await getTodayStats();
+        console.log(response.code === '4010');
+        if (response.code === '4010') {
+          router.replace('/not-found');
+        } else {
+          setChecked(true);
+        }
+      } catch (e) {
+        console.error(e);
+        router.replace('/not-found');
+      }
+    };
+
+    checkAccess();
+  }, []);
+
+  if (!checked) return null;
+
   return (
     <>
       <TodayStats />
