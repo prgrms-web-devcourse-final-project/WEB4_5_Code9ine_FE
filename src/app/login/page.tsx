146f01d9115a2dfc9d82a37ed76eea2153c27fa1
manto123123123 <lhw971103@gmail.com>
2025-07-28 12:31:59 +0900

    :Feat 로그인 회원가입 로고추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,33 @@
+'use client';
+
 import Loginbox from '@/components/login/loginbox';
+import logo from '@/assets/Logo.svg';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Login() {
   const gradient = `
-  radial-gradient(
-    circle,
-    var(--login-gradient-color-1) 0%,
-    var(--login-gradient-color-2) 32%,
-    var(--login-gradient-color-3) 70%,
-    var(--login-gradient-color-3) 100%
-  )
-`;
+    radial-gradient(
+      circle,
+      var(--login-gradient-color-1) 0%,
+      var(--login-gradient-color-2) 32%,
+      var(--login-gradient-color-3) 70%,
+      var(--login-gradient-color-3) 100%
+    )
+  `;
+
   return (
     <div
-      className="mx-auto flex h-screen w-full items-center justify-center rounded-[20px] md:h-[900px] md:w-[1366px]"
+      className="mx-auto flex h-screen w-full flex-col items-center justify-center rounded-[20px] md:h-[900px] md:w-[1366px]"
       style={{ background: gradient }}
     >
+      <Link href={'/'}>
+        <div className="mb-[20px] flex h-[29px] w-[87px] cursor-pointer items-center justify-center gap-[13px]">
+          <Image src={logo} alt="티태 로고" />
+          <span className="text-[18px] font-semibold text-[#ffffff]">티태</span>
+        </div>
+      </Link>
+
       <Loginbox />
     </div>
   );
