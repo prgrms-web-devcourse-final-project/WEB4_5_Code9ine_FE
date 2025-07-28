80480c3ddebf1b1a6c719143a77e03684112cd45
manto123123123 <lhw971103@gmail.com>
2025-07-28 11:36:54 +0900

    :Refactor 이름 수정

--- src/components/main/AuthorizedMain.tsx
+++ src/components/main/AuthorizedMain.tsx
@@ -42,7 +42,7 @@ export default function AuthorizedMain() {
       <div className="hidden md:flex md:flex-col md:gap-[20px]">
         <div className="h-[390px] w-[350px] rounded-[10px] bg-[var(--white-color)] shadow-[var(--shadow-md)]">
           <TitleSwiper />
-          <Profile isPersonal={true} userData={userData} />
+          <Profile isPersonal={true} />
         </div>
         <div className="h-[460px] w-[350px] rounded-[10px] bg-[var(--white-color)] shadow-[var(--shadow-md)]">
           <Mission challengeList={challengeList} />
