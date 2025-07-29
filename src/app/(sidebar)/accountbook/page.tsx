05c18ea87b3db4a4d46df8dd8c2032286cb7a4f5
SeolWolWha <leeyoon97@naver.com>
2025-07-30 01:32:34 +0900

    Design: select-none 적용

--- src/app/(sidebar)/accountbook/page.tsx
+++ src/app/(sidebar)/accountbook/page.tsx
@@ -6,7 +6,7 @@ import Page from '@/components/accountbook/Page';
 
 export default async function page() {
   return (
-    <div className="h-[702px] min-w-[350px] md:h-[870px] md:w-[1121px]">
+    <div className="h-[702px] min-w-[350px] select-none md:h-[870px] md:w-[1121px]">
       <Page />
     </div>
   );
